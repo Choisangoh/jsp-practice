8fef847fc13d31426643934b00fe54c667812415
Choisangoh <96283032+Choisangoh@users.noreply.github.com>
2022-03-03 12:08:35 +0900

    Add files via upload

--- MyFirstWeb/src/main/java/kr/co/ict/servlet/ServeltCustom.java
+++ MyFirstWeb/src/main/java/kr/co/ict/servlet/ServeltCustom.java
@@ -1,6 +1,9 @@
 package kr.co.ict.servlet;
 
 import java.io.IOException;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -34,7 +37,7 @@ public class ServeltCustom extends HttpServlet {
 	 * @see Servlet#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		System.out.println("/visit 접속 시 생성된 객체는 서버 종료로 인해 파기된다.");
 	}
 
 	/**
@@ -51,17 +54,48 @@ public class ServeltCustom extends HttpServlet {
 		
 		// "jpa"라는 이름으로 들어온 요소를 콘솔에 찍도록 추가하기
 		String jpa = request.getParameter("jpa");
-		System.out.println(jpa);
+		System.out.println(jpa);	
 		
-		
+		// 리다이렉트 방식은 페이지만 이동하고 데이터는 같이 전송하지 않는다.
+		// response.sendRedirect("http://localhost:8181/MyFirstWeb/servletForm/visitResult.jsp");
+	    
+		// 포워딩 하기 전 request에 setAttribute();를 이용해 데이터를 저장한다.
+		request.setAttribute("jsp", jsp);
+		request.setAttribute("boot", boot);
+		request.setAttribute("jpa", jpa);
+		// 상단 변수를 함께 전달하기 위해 forward를 사용한다.
+		// 목적지 주소는 'localhost:포트번호/프로젝트명'는 제외하고,
+		// '/이후경로'만 입력하면 된다.
+		RequestDispatcher dp = request.getRequestDispatcher("/servletForm/visitResult.jsp");
+		dp.forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// doPost는 현재까지 배운 내용만 놓고 봤을 때, form에서 post방식으로 제출할떄 호출된다.
+		System.out.println("/visit 주소 post방식으로 접속함");
+		// 한글 안꺠지도록 설정
+		request.setCharacterEncoding("utf-8");
+		String jsp = request.getParameter("jsp");
+		String boot = request.getParameter("boot");
+		String jpa = request.getParameter("jpa");
+		System.out.println("post방식 : " + jsp);
+		System.out.println("post방식 : " + boot);
+		System.out.println("post방식 : " + jpa);
+		
+		// 리다이렉트 방식은 페이지만 이동하고 데이터는 같이 전송하지 않는다.
+		// response.sendRedirect("http://localhost:8181/MyFirstWeb/servletForm/visitResult.jsp");
+		
+		// 포워딩 하기 전 request에 setAttribute();를 이용해 데이터를 저장한다.
+		request.setAttribute("jsp", jsp);
+		request.setAttribute("boot", boot);
+		request.setAttribute("jpa", jpa);
+		// 상단 변수를 함께 전달하기 위해 forward를 사용한다.
+		// 목적지 주소는 'localhost:포트번호/프로젝트명'는 제외하고,
+		// '/이후경로'만 입력하면 된다.
+		RequestDispatcher dp = request.getRequestDispatcher("/servletForm/visitResult.jsp");
+		dp.forward(request, response);
 	}
-
 }
--- MyFirstWeb/src/main/webapp/servletForm/visitForm.jsp
+++ MyFirstWeb/src/main/webapp/servletForm/visitForm.jsp
@@ -8,7 +8,7 @@
 </head>
 <body>
    <!-- servlet으로 보내기 위해서는 action에 servlet 접속 url 작성-->
-   <form action="http://localhost:8181/MyFirstWeb/visit" method="get">
+   <form action="http://localhost:8181/MyFirstWeb/visit" method="post">
       <input type="text" name="jsp" placeholder="jsp"><br/>
       <input type="text" name="boot" placeholder="boot"><br/>
       <input type="text" name="jpa" placeholder="jpa"><br/>
